0827d6b003fa8b0f416971647a3657922091bf58
rock-dengpaoer <xdt0916@qq.com>
2021-05-17 16:29:46 +0800

    Create 47.c

---
+++ 47/47.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define exchange(a,b) {int t;t=a;a=b;b=t;}
+int main()
+{
+	int x=10;
+	int y=20;
+	printf("x=%d,y=%d\n",x,y);
+	exchange(x,y);
+	printf("x=%d,y=%d\n",x,y);
+	return 0;
+}
