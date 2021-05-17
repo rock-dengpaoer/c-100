adc2bf485bae3276dba343b421f89a168b9e325c
rock-dengpaoer <xdt0916@qq.com>
2021-05-17 16:10:26 +0800

    43

---
+++ 43/43.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i,num;
+	num=2;
+	for(i=0;i<3;i++)
+	{
+		printf("num is%d\n",num);
+		num++;
+		{
+			static int num=1;
+			printf("behind num is %d\n",num);
+			num++;
+		}
+	}
+	return 0;
+}
