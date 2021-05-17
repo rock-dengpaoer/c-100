46f71ad004b23aa80f86124e3e09c946db6cd9dc
rock-dengpaoer <xdt0916@qq.com>
2021-05-17 15:19:00 +0800

    38

---
+++ 38/38.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define N 3
+int main()
+{
+	int i,j,a[N][N],sum=0;
+	printf("please input:\n");
+	for(i=0;i<N;i++)
+		for(j=0;j<N;j++)
+			scanf("%d",&a[i][j]);
+	for(i=0;i<N;i++)
+		sum+=a[i][i];
+	printf("result is %d\n",sum);
+	return 0;
+}
