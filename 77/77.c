beab23a19b13a6ba69eaa065c9fcb0ae2c4249e6
rock-dengpaoer <xdt0916@qq.com>
2021-05-18 10:14:59 +0800

    Create 77.c

---
+++ 77/77.c
@@ -0,0 +1,15 @@
+// 填空练习（指向指针的指针）。
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    const char *s[]={"man","woman","girl","boy","sister"};
+    const char **q;
+    int k;
+    for(k=0;k<5;k++)
+    {
+        q=&s[k];       /*在这里填入内容*/
+        printf("%s\n",*q);
+    }
+    return 0;
+}
